12b580e101072d70ebe90daff42fafccd230d3e9
nakagawahina <152489046+nakagawahina@users.noreply.github.com>
2024-08-02 10:26:39 +0900

    Dev18 (#19)
    * footer完了 #18
    
    * 完了 #18

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import style from "./Footer.module.css";
+
+function Footer() {
+  return (
+  <footer>
+    <div className={style.test}>
+      <p className={style.character}>&copy; 2024 Joken</p>
+    </div>
+  </footer>
+  );
+}
+export default Footer;
