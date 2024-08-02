604def3f0457a7b45210d20444ad42b03eeeb477
tacotaco0808 <111967005+tacotaco0808@users.noreply.github.com>
2024-08-02 12:15:06 +0900

    Dev12 (#32)
    * スタンプボード以外完了 #dev12
    
    * sassに変更
    
    * #12 ホーム画面のcss適用

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,34 +1,56 @@
-import style from "./page.module.css";
+import Footer from "../components/Footer";
+import style from "./page.module.scss";
 import Image from "next/image";
 function Home() {
   return (
     <div className={style.home}>
       <header>header</header>
+      <div className={style.emptybox}></div>
       <div className={style.tutorial}>
         <h2>アプリの使い方</h2>
-        <div className={style.tutorial_item}>
-          <Image src="/mock/100x100.png" alt="test" height={100} width={100} />
-          <p className={style.tutorial_description}>
-            地図上のQRコードを読み取ってパネルをゲット
-          </p>
-        </div>
-        <div className={style.tutorial_item}>
-          <Image src="/mock/100x100.png" alt="test" height={100} width={100} />
-          <p className={style.tutorial_description}>
-            合計9枚のパネルを集めてスタンプボードの絵を完成させよう
-          </p>
-        </div>
-        <div className={style.tutorial_item}>
-          <Image src="/mock/100x100.png" alt="test" height={100} width={100} />
-          <p className={style.tutorial_description}>
-            完成した絵をスタッフに見せて景品ゲット
-          </p>
+        <div className={style.tutorial_item__wrapper}>
+          <div className={style.tutorial_item}>
+            <Image
+              className={style.tutorial_image}
+              src="/mock/100x100.png"
+              alt="test"
+              height={100}
+              width={100}
+            />
+            <p className={style.tutorial_description}>
+              地図上のQRコードを読み取ってパネルをゲット
+            </p>
+          </div>
+          <div className={style.tutorial_item}>
+            <Image
+              className={style.tutorial_image}
+              src="/mock/100x100.png"
+              alt="test"
+              height={100}
+              width={100}
+            />
+            <p className={style.tutorial_description}>
+              合計9枚のパネルを集めてスタンプボードの絵を完成させよう
+            </p>
+          </div>
+          <div className={style.tutorial_item}>
+            <Image
+              className={style.tutorial_image}
+              src="/mock/100x100.png"
+              alt="test"
+              height={100}
+              width={100}
+            />
+            <p className={style.tutorial_description}>
+              完成した絵をスタッフに見せて景品ゲット
+            </p>
+          </div>
         </div>
       </div>
       <div className={style.stump}>
         <h2>スタンプボード</h2>
       </div>
-      <footer>footer</footer>
+      <Footer />
     </div>
   );
 }
