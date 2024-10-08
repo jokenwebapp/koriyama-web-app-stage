da68e5dcf28e072ffaeb1056b54741222f802699
nakagawahina <152489046+nakagawahina@users.noreply.github.com>
2024-10-09 00:48:44 +0900

    学生の声実装 (#63)
    * 学生の声実装
    
    * git pull

--- frontend/src/components/PlaceReviewCards.tsx
+++ frontend/src/components/PlaceReviewCards.tsx
@@ -1,41 +1,22 @@
 'use client';
 import Image from 'next/image';
-import style from './PlaceReviewCards.module.scss'; /*cssを使用しデザインを変えたい場合は、←のファイルを編集すること */
+import style from './PlaceReviewCards.module.scss';
 
-/* studentVoiceList(StudentVoice型の配列)として、学生の声に表示する内容を受け取る */
 function PlaceReviewCards({ studentVoiceList }: { studentVoiceList: StudentVoice[] }) {
-  // studentVoiceの中身を参照する(不要であれば削除可)
-  /*
-  console.log('studentVoiceList:', studentVoiceList);
-  console.log('studentVoiceList[0]', studentVoiceList[0]);
-  console.log('studentVoiceList[0].id', studentVoiceList[0].id);
-  console.log('studentVoiceList[0].academic_year', studentVoiceList[0].academic_year);
-  console.log('studentVoiceList[0].description', studentVoiceList[0].description);
-  */
+  
+  //console.log('studentVoiceList:', studentVoiceList);
 
-  /* 以下をstudentVoiceListの値に応じて、内容を変化させるように書き換える */
   return (
     <div className={style.place_review__cards}>
-      <div className={style.place_review__card}>
-        <div className={style.place_review__studentinfo}>
-          <Image src="/images/student_icon.png" alt="" width={69} height={69} />
-          <h4>学部1年</h4>
+      {studentVoiceList.map((voice, index) => (
+        <div key={index} className={style.place_review__card}>
+          <div className={style.place_review__studentinfo}>
+            <Image src="/images/student_icon.png" alt="Student Icon" width={69} height={69} />
+            <h4>{voice.academic_year}</h4>
+          </div>
+          <p>{voice.description}</p>
         </div>
-        <p>
-          学生から見た施設のおすすめポイント学生から見た施設のおすすめポイント学生から見た施設のおすすめポイント
-        </p>
-      </div>
-
-      <div className={style.place_review__card}>
-        <div className={style.place_review__studentinfo}>
-          <Image src="/images/student_icon.png" alt="" width={69} height={69} />
-          <h4>学部1年</h4>
-        </div>
-        <p>
-          学生から見た施設のおすすめポイント学生から見た施設のおすすめポイント学生から見た施設のおすすめポイント
-        </p>
-      </div>
-      {/* 参考： <p>{studentVoiceList[0].description}</p> のよう{}を使うことで、タグ内で変数が参照できる*/}
+      ))}
     </div>
   );
 }
